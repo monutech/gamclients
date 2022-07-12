468a44b13f2097690fd43ef58f5bfdf82f19a168
DJ Gillespie <dj@monumetric.com>
2022-07-12 10:00:59 -0600

    updates to requirements for 0.0.4.1 release

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gamclients",
-    version="0.0.4",
+    name="admanager-reports",
+    version="0.0.4.1",
     author="Monumetric",
     author_email="tech@monumetric.com",
     description="A small wrapper for the GAM python API to enable easier " +
@@ -20,5 +20,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    install_requires = ['tqdm', 'googleads', 'oauth2client', 'pandas', 'ipython', 'polars'],
+    install_requires = ['tqdm==4.64.0', 'googleads==32.0.0', 'oauth2client==4.1.3', 'pandas==1.4.3', 'polars==0.13.51'],
 )
